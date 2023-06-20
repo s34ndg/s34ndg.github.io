cde26663972b99976b3dc4dde5e9755de8853ff4
s34ndg <s34ndg@gmail.com>
2023-06-20 14:35:10 -0500

    99

--- audio-worklet.js
+++ audio-worklet.js
@@ -1,93 +1,61 @@
-// Create a new AudioWorkletProcessor class for your custom audio processing
-class MyAudioWorkletProcessor extends AudioWorkletProcessor {
-  process(inputs, outputs, parameters) {
-    const input = inputs[0]; // Assuming single input channel
-    const output = outputs[0]; // Assuming single output channel
-
-    const inputChannel = input[0];
-    const outputChannel = output[0];
-
-    const bufferSize = inputChannel.length;
-
-    // Process each sample in the input channel
-    for (let i = 0; i < bufferSize; i++) {
-      const sample = inputChannel[i];
-
-      // Apply your audio processing algorithm here
-      // Modify the sample value as needed
-      // For example, you can apply filters, effects, or manipulate the audio data in some way
-
-      // Apply a simple gain to the input sample
-      const gain = 2; // Adjust the gain value as needed
-      const processedSample = sample * gain;
-
-      // Write the processed sample to the output channel
-      outputChannel[i] = processedSample;
-    }
-
-    return true;
-  }
-}
-
 // Create a new SilenceDetector class for your silence detection
 class SilenceDetector extends AudioWorkletProcessor {
-  constructor() {
-    super();
-    this.threshold = -30; // default threshold value
-    this.silenceDuration = 3; // default silence duration in seconds
-    this.currentAudioLevel = 0;
-    this.silenceTimer = null;
-    this.port.onmessage = this.handleMessage.bind(this);
-  }
-
-  handleMessage(event) {
-    const { data } = event;
-    if (data.threshold !== undefined) {
-      this.threshold = data.threshold;
-    }
-    if (data.silenceDuration !== undefined) {
-      this.silenceDuration = data.silenceDuration;
-    }
-  }
-
-  process(inputs, outputs) {
-    const input = inputs[0];
-    let sumSquared = 0;
-
-    // Calculate the sum of squared values for all channels
-    for (let channel = 0; channel < input.length; ++channel) {
-      const inputChannel = input[channel];
-      for (let i = 0; i < inputChannel.length; ++i) {
-        sumSquared += inputChannel[i] * inputChannel[i];
-      }
+    constructor() {
+        super();
+        this.threshold = -30; // default threshold value
+        this.silenceDuration = 3; // default silence duration in seconds
+        this.currentAudioLevel = 0;
+        this.silenceTimer = null;
+        this.port.onmessage = this.handleMessage.bind(this);
     }
 
-    // Calculate the average power level
-    const rms = Math.sqrt(sumSquared / input[0].length);
-
-    // Convert the average power level to decibels (dB)
-    const dB = 20 * Math.log10(rms);
-
-    this.currentAudioLevel = dB;
-
-    // Check if the current audio level is below the threshold
-    if (dB < this.threshold) {
-      // If silence timer is not running, start it
-      if (!this.silenceTimer) {
-        this.silenceTimer = setTimeout(() => {
-          this.port.postMessage('silence-detected');
-        }, this.silenceDuration * 1000);
-      }
-    } else {
-      // If audio level is above the threshold, clear the silence timer
-      clearTimeout(this.silenceTimer);
-      this.silenceTimer = null;
+    handleMessage(event) {
+        const { data } = event;
+        if (data.threshold !== undefined) {
+            this.threshold = data.threshold;
+        }
+        if (data.silenceDuration !== undefined) {
+            this.silenceDuration = data.silenceDuration;
+        }
     }
 
-    return true;
-  }
+    process(inputs, outputs) {
+        const input = inputs[0];
+        let sumSquared = 0;
+
+        // Calculate the sum of squared values for all channels
+        for (let channel = 0; channel < input.length; ++channel) {
+            const inputChannel = input[channel];
+            for (let i = 0; i < inputChannel.length; ++i) {
+                sumSquared += inputChannel[i] * inputChannel[i];
+            }
+        }
+
+        // Calculate the average power level
+        const rms = Math.sqrt(sumSquared / input[0].length);
+
+        // Convert the average power level to decibels (dB)
+        const dB = 20 * Math.log10(rms);
+
+        this.currentAudioLevel = dB;
+
+        // Check if the current audio level is below the threshold
+        if (dB < this.threshold) {
+            // If silence timer is not running, start it
+            if (!this.silenceTimer) {
+                this.silenceTimer = setTimeout(() => {
+                    this.port.postMessage('silence-detected');
+                }, this.silenceDuration * 1000);
+            }
+        } else {
+            // If audio level is above the threshold, clear the silence timer
+            clearTimeout(this.silenceTimer);
+            this.silenceTimer = null;
+        }
+
+        return true;
+    }
 }
 
-// Register the audio worklet processors
-registerProcessor('my-audio-worklet-processor', MyAudioWorkletProcessor);
+// Register the audio worklet processor
 registerProcessor('silence-detector', SilenceDetector);
